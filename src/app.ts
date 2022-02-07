a01e023431ea49da700ae54274449496261bd26e
Jaikishan Jalan <jjalan@jalantechnologies.com>
2022-02-07 10:06:52 +0530

    REST api server

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application } from 'express';
+import { Server } from 'http';
+
+export default class App {
+  private static app: Application;
+
+  public static startRESTApiServer(): Server {
+    this.app = express();
+    // The order of core services is important because Logger depends on Config
+    // await ConfigManager.mountConfig()
+    // await LoggerManager.mountLogger()
+    // await CommunicationServiceManager.mountService()
+
+    // Mount micro services, the order is not important here
+    // const accountServiceRESTApi = AccountServiceManager.createRestAPIServer()
+    // this.app.use('/', accountServiceRESTApi)
+
+    // const accessTokenServiceRESTApi = AccessTokenServiceManager.createRestAPIServer()
+    // this.app.use('/', accessTokenServiceRESTApi)
+
+    // const taskServiceRESTApi = TaskServiceManager.createRestAPIServer()
+    // this.app.use('/', taskServiceRESTApi)
+
+    // TODO: Read the port from ConfigService
+    return this.app.listen(8000);
+  }
+}
