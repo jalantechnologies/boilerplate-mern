5146197f89d8a8a7cbe447387916a3e0ef49d9c2
Sparsh Kumar <sparshkumar@Sparshs-MacBook-Pro.local>
2022-02-08 17:33:35 +0530

    sms service completed.

---
+++ src/modules/communication/internals/twilio-params.ts
@@ -0,0 +1,42 @@
+import {
+  PhoneNumber, SendSMSParams, ValidationError, Failure
+} from "../types";
+import { PhoneNumberUtil } from 'google-libphonenumber';
+
+export class SmsParams {
+  phoneNumber: PhoneNumber;
+  message: string;
+  constructor(params: SendSMSParams) {
+    this.phoneNumber = params.to;
+    this.message = params.message;
+  }
+  phoneNumberString() {
+    return `${this.phoneNumber.countryCode}${this.phoneNumber.phoneNumber}`;
+  }
+  async validate() {
+    const phoneUtil = PhoneNumberUtil.getInstance();
+    const failures: Failure[] = [];
+    const isMessageValid = !!this.message;
+    const isPhoneNumberValid = phoneUtil.isValidNumber(
+      phoneUtil.parse(`${this.phoneNumberString()}`)
+    );
+    if(!isMessageValid) {
+      failures.push({
+        field: 'message',
+        message: 'please specify non empty message body',
+      });
+    }
+    if(!isPhoneNumberValid) {
+      failures.push({
+        field: 'to',
+        message: 'Please specify valid phone number to send sms to.',
+      });
+    }
+    if(failures.length) {
+      throw new ValidationError(
+        'Sms sent failed, please provide valid params.',
+        failures
+      );
+    }
+  }
+};
---
+++ test/spec/sms/twilio.spec.ts
@@ -0,0 +1,70 @@
+require('module-alias/register');
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { Server } from 'http';
+import sinon from 'sinon';
+import { Application } from 'express';
+import {
+  SMSService
+} from '../../../src/modules/communication';
+import { initApp } from '../../helpers/spec-helpers';
+
+chai.use(chaiHttp);
+let expressApp: Application;
+let server: Server;
+let sinonSandbox: sinon.SinonSandbox;
+
+describe('SMS Service', function() {
+
+  before(async function() {
+    expressApp = await initApp();
+    server = await expressApp.startRESTApiServer();
+    sinonSandbox = sinon.createSandbox();
+  });
+
+  after(async function() {
+    server.close();
+  });
+
+  afterEach(() => {
+    sinonSandbox.restore();
+  });
+
+  it('should not allow to send sms, if the reciepient phone number is invalid', async function() {
+    const params = {
+      to: {
+        countryCode: '+91',
+        phoneNumber: '73001',
+      },
+      message: 'Sample email.'
+    };
+    let errorOccured: any;
+    try {
+      await SMSService.sendSMS(params);
+    } catch(e) {
+      errorOccured = e;
+    }
+    expect(errorOccured.msg).to.eq('Sms sent failed, please provide valid params.');
+    expect(errorOccured.failures.length).to.eq(1);
+    expect(errorOccured.failures[0].field).to.eq('to');
+  });
+
+  it('should not allow to send sms, if the message body is empty.', async function() {
+    const params = {
+      to: {
+        countryCode: '+91',
+        phoneNumber: '7300159158',
+      },
+      message: ''
+    };
+    let errorOccured: any;
+    try {
+      await SMSService.sendSMS(params);
+    } catch(e) {
+      errorOccured = e;
+    }
+    expect(errorOccured.msg).to.eq('Sms sent failed, please provide valid params.');
+    expect(errorOccured.failures.length).to.eq(1);
+    expect(errorOccured.failures[0].field).to.eq('message');
+  });
+});
