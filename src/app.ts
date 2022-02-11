126412b3493c4bd62a2458580671a493005e0214
Akash Lakhera <akashlakhera05@gmail.com>
2022-02-11 18:15:15 +0530

    add comm service to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Server } from 'http';
 import serverErrorHandler from './error-handler';
 import AccesstokenServiceManager from './modules/access-token/access-token-manager';
 import AccountServiceManager from './modules/account/account-service-manager';
+import CommunicationServiceManager from './modules/communication/communication-service-manager';
 import ConfigManager from './modules/config/config-manager';
 import ConfigService from './modules/config/config-service';
 import LoggerManager from './modules/logger/logger-manager';
@@ -16,7 +17,7 @@ export default class App {
     // Core Services
     await ConfigManager.mountConfig();
     await LoggerManager.mountLogger();
-    // await CommunicationServiceManager.mountService()
+    await CommunicationServiceManager.mountService();
 
     // Micro Services
     const accountServiceRESTApi = await AccountServiceManager.createRestAPIServer();
