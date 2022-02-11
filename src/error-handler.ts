fa5dc2a11ce2202160e296c565eff219516b0055
Akash Lakhera <akashlakhera05@gmail.com>
2022-02-11 22:28:51 +0530

    types for error middleware

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,8 +1,15 @@
-import { ErrorRequestHandler } from 'express';
+import {
+  ErrorRequestHandler, Response, Request, NextFunction,
+} from 'express';
 import AppError from './modules/app-error/app-error';
 import Logger from './modules/logger/logger';
 
-const serverErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+const serverErrorHandler: ErrorRequestHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
   const error = err as AppError;
   const errorString = error.toString();
   const errorJson = error.toJson();
