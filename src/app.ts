6a9d77118997eb971d75b4f18f648279722a647b
Akash Lakhera <akashlakhera05@gmail.com>
2022-02-11 16:22:06 +0530

    mono app

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
 import express, { Application } from 'express';
 import { Server } from 'http';
+import AccesstokenServiceManager from './modules/access-token/access-token-manager';
+import AccountServiceManager from './modules/account/account-service-manager';
+import ConfigManager from './modules/config/config-manager';
+import ConfigService from './modules/config/config-service';
+import LoggerManager from './modules/logger/logger-manager';
 
 export default class App {
   private static app: Application;
 
-  public static startRESTApiServer(): Server {
+  public static async startRESTApiServer(): Promise<Server> {
     this.app = express();
-    // The order of core services is important because Logger depends on Config
-    // await ConfigManager.mountConfig()
-    // await LoggerManager.mountLogger()
+
+    // Core Services
+    await ConfigManager.mountConfig();
+    await LoggerManager.mountLogger();
     // await CommunicationServiceManager.mountService()
 
-    // Mount micro services, the order is not important here
-    // const accountServiceRESTApi = AccountServiceManager.createRestAPIServer()
-    // this.app.use('/', accountServiceRESTApi)
+    // Micro Services
+    const accountServiceRESTApi = await AccountServiceManager.createRestAPIServer();
+    this.app.use('/', accountServiceRESTApi);
 
-    // const accessTokenServiceRESTApi = AccessTokenServiceManager.createRestAPIServer()
-    // this.app.use('/', accessTokenServiceRESTApi)
+    const accessTokenServiceRESTApi = await AccesstokenServiceManager.createRestAPIServer();
+    this.app.use('/', accessTokenServiceRESTApi);
 
-    // const taskServiceRESTApi = TaskServiceManager.createRestAPIServer()
-    // this.app.use('/', taskServiceRESTApi)
-
-    // TODO: Read the port from ConfigService
-    return this.app.listen(8000);
+    const port = ConfigService.getIntValue('server.port');
+    return this.app.listen(port);
   }
 }
