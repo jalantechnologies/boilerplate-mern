1ce3630390fc579ca7e203ea8d12101b98011f92
Akash Lakhera <akashlakhera05@gmail.com>
2022-02-11 15:00:25 +0530

    remove boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -15,18 +15,3 @@ function tick() {
 }
 
 tick();
-import express from 'express';
-import AccountServiceManager from './modules/account/account-service-manager';
-import AccesstokenServiceManager from './modules/access-token/access-token-manager';
-async function init() {
-  const server = await AccesstokenServiceManager.createRestAPIServer();
-  const s2 = await AccountServiceManager.createRestAPIServer();
-  const app = express();
-  app.use('/', server);
-  app.use('/', s2);
-  app.listen(3000, () => {
-    console.log('Rest api server is running on port 3000');
-  });
-}
-
-init();
