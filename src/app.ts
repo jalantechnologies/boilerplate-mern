e6ad9deb73ca6792ab9eee25ced3fb5f8e20edd5
Sparsh Kumar <sparshkumar@Sparshs-MacBook-Pro.local>
2022-02-14 11:32:22 +0530

    done suggested changes in PR.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Application } from 'express';
 import { Server } from 'http';
 import serverErrorHandler from './error-handler';
-import MicroServices from './load-services';
+import AccesstokenServiceManager from './modules/access-token/access-token-manager';
+import AccountServiceManager from './modules/account/account-service-manager';
 import CommunicationServiceManager from './modules/communication/communication-service-manager';
 import ConfigManager from './modules/config/config-manager';
 import ConfigService from './modules/config/config-service';
@@ -9,6 +10,18 @@ import LoggerManager from './modules/logger/logger-manager';
 
 export default class App {
   private static app: Application;
+
+  private static async createRESTApiServer(): Promise<Application> {
+    const app = express();
+
+    const accountServiceRESTApi = await AccountServiceManager.createRestAPIServer();
+    app.use('/', accountServiceRESTApi);
+
+    const accessTokenServiceRESTApi = await AccesstokenServiceManager.createRestAPIServer();
+    app.use('/', accessTokenServiceRESTApi);
+
+    return app;
+  }
 
   public static async startRESTApiServer(): Promise<Server> {
     this.app = express();
@@ -19,8 +32,7 @@ export default class App {
     await CommunicationServiceManager.mountService();
 
     // loading all microservices
-    // all microservices would listen on /api endpoint now.
-    const microServicesApi = await MicroServices.loadMicroServices();
+    const microServicesApi = await this.createRESTApiServer();
     this.app.use('/api', microServicesApi);
 
     // Error handling
