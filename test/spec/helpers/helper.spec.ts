289fd4549ca0f2a91874c0902d429314dc1efbfe
Akash Lakhera <akashlakhera05@gmail.com>
2022-02-23 05:25:37 +0530

    remove log

--- test/spec/helpers/helper.spec.ts
+++ test/spec/helpers/helper.spec.ts
@@ -1,15 +1,7 @@
 import App from '../../../src/app';
-import AccountRepository from '../../../src/modules/account/internal/store/account-repository';
-import TaskRepository from '../../../src/modules/task/internal/store/task-repository';
 
 export let app;
 
 before(async () => {
-  console.log('starting server');
   app = await App.startRESTApiServer();
-});
-
-after(async () => {
-  await AccountRepository.accountDB.deleteMany();
-  await TaskRepository.taskDB.deleteMany();
 });
