4046dae6a6c86077b3175ad5569790dddd4da3e4
Himanshu Sahu <himanshu.sahu@jalantechnologies.com>
2022-08-01 23:13:14 +0530

    Added Inspectlet script

--- src/public/js/ui/app.tsx
+++ src/public/js/ui/app.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { LoginForm } from './components/login-form';
+import InspectLet from '../../../utils/Inspect';
 
 export const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Helmet>
+        <script src="./Inspect.js" type="text/javascript">
+          {InspectLet()}
+        </script>
+      </Helmet>
       <main>
         <div class="container flex-shrink-0">
           <h1 class="mt-5">Home</h1>
---
+++ src/utils/Inspect.ts
@@ -0,0 +1,26 @@
+export {};
+
+declare global {
+  interface Window {
+    __insp: any
+  }
+}
+
+const InspectLet = () => {
+    window.__insp = window.__insp || [];
+    __insp.push(['wid', 433167548]);
+    var ldinsp = function() {
+        if (typeof window.__inspld != "undefined") return;
+        window.__inspld = 1;
+        var insp = document.createElement('script');
+        insp.type = 'text/javascript';
+        insp.async = true;
+        insp.id = "inspsync";
+        insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=433167548&r=' + Math.floor(new Date().getTime() / 3600000);
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(insp, x);
+    };
+    setTimeout(ldinsp, 0);
+}
+
+export default InspectLet;
