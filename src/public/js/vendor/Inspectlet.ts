9cdd3eab8d754d5442adc9003c07253fb3bb6eb5
Himanshu Sahu <himanshu.sahu@jalantechnologies.com>
2022-08-02 21:34:09 +0530

    Linting issues fixed

--- src/public/js/vendor/Inspectlet.ts
+++ src/public/js/vendor/Inspectlet.ts
@@ -4,30 +4,29 @@ const wid = 812310448;
 declare global {
   interface Window {
     __insp: Array<Array<string | number>>,
-    __inspld: Number
+    __inspld: number
   }
 }
 
-const InspectLet = () => {
-    window.__insp = window.__insp || [];
-    window.__insp.push(['wid', wid]);
-    var ldinsp = function() {
-      if (typeof window.__inspld != "undefined") return;
-      window.__inspld = 1;
-      var insp = document.createElement('script');
-      insp.type = 'text/javascript';
-      insp.async = true;
-      insp.id = "inspsync";
-      insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + `://cdn.inspectlet.com/inspectlet.js?wid=${wid}&r=` + Math.floor(new Date().getTime() / 3600000);
-      var x = document.getElementsByTagName('script')[0];
-      if(x===undefined){
-        document.getElementsByTagName('head')[0].appendChild(insp);
-      }
-      else{
-        x.parentNode.insertBefore(insp, x);
-      }
-    };
-    setTimeout(ldinsp, 0);
-}
+const InspectLet = () : void => {
+  window.__insp = window.__insp || [];
+  window.__insp.push(['wid', wid]);
+  const ldinsp = () : void => {
+    if (typeof window.__inspld !== 'undefined') return;
+    window.__inspld = 1;
+    const insp = document.createElement('script');
+    insp.type = 'text/javascript';
+    insp.async = true;
+    insp.id = 'inspsync';
+    insp.src = `${document.location.protocol === 'https:' ? 'https' : 'http'}://cdn.inspectlet.com/inspectlet.js?wid=${wid}&r=${Math.floor(new Date().getTime() / 3600000)}`;
+    const x = document.getElementsByTagName('script')[0];
+    if (x === undefined) {
+      document.getElementsByTagName('head')[0].appendChild(insp);
+    } else {
+      x.parentNode.insertBefore(insp, x);
+    }
+  };
+  setTimeout(ldinsp, 0);
+};
 
 export default InspectLet;
