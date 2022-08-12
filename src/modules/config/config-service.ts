a31ac0466347d6c23b057352c82a851ee1f71da2
Ankit Bhawnani <bbbneo333@gmail.com>
2022-08-12 12:56:59 +0530

    feat: updated ConfigService, added hasValue

--- src/modules/config/config-service.ts
+++ src/modules/config/config-service.ts
@@ -5,25 +5,29 @@ import { MissingKeyError, ValueTypeMismatchError } from './types';
 export default class ConfigService {
   public static getBoolValue(key: string): boolean {
     const expectedValueType = ConfigType.BOOLEAN;
-    return this.getEnvValue<boolean>(expectedValueType, key);
+    return this.getValue<boolean>(expectedValueType, key);
   }
 
   public static getIntValue(key: string): number {
     const expectedValueType = ConfigType.NUMBER;
-    return this.getEnvValue<number>(expectedValueType, key);
+    return this.getValue<number>(expectedValueType, key);
   }
 
   public static getStringValue(key: string): string {
     const expectedValueType = ConfigType.STRING;
-    return this.getEnvValue<string>(expectedValueType, key);
+    return this.getValue<string>(expectedValueType, key);
   }
 
   public static getListValue<K>(key: string): K[] {
     const expectedValueType = ConfigType.OBJECT;
-    return this.getEnvValue<K[]>(expectedValueType, key);
+    return this.getValue<K[]>(expectedValueType, key);
   }
 
-  private static getEnvValue<T>(expectedValueType: ConfigType, key: string): T {
+  public static hasValue(key: string): boolean {
+    return config.has(key);
+  }
+
+  private static getValue<T>(expectedValueType: ConfigType, key: string): T {
     const value = config.get(key);
 
     if (value === undefined) {
