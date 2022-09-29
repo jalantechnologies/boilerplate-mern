9a8171ce4216cde6be819bd556a8179d3fd920e8
Akash Lakhera <akashlakhera05@gmail.com>
2022-09-29 12:14:59 +0530

    fix build

--- src/public/js/ui/components/footer.tsx
+++ src/public/js/ui/components/footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-        <span class="text-muted">&#169; Jalan Technology Consulting, 2022</span>
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <span className="text-muted">&#169; Jalan Technology Consulting, 2022</span>
       </div>
     </footer>
   );
---
+++ src/types/globals.d.ts
@@ -0,0 +1,13 @@
+/*
+Copyright (c) Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// TODO: Fix baseui typescript issue and remove this file
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare type TimeoutID = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare type AnimationFrameID = any;
