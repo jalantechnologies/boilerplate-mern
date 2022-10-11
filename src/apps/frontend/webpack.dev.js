6adcf35e4e2ee00aff8b0c43db48c3663070f98f
Ankit Bhawnani <ankit@jalantechnologies.com>
2022-10-11 18:45:40 +0530

    Fix webpack-dev-server routing issues (#39)
    fix: fixed webpack dev server

--- src/apps/frontend/webpack.dev.js
+++ src/apps/frontend/webpack.dev.js
@@ -12,11 +12,13 @@ const config = {
   },
   devtool: 'inline-source-map',
   devServer: {
+    historyApiFallback: true,
     hot: true,
     open: true,
     port: 3000,
     proxy: {
       '/api': 'http://localhost:8080',
+      '/assets': 'http://localhost:8080',
     },
   },
 };
