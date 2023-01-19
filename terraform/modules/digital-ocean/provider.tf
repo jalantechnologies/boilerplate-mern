91f3eadb37be9396d2fb9cb43c2c5266b91833dd
Ankit Bhawnani <bbbneo333@gmail.com>
2023-01-19 13:01:17 +0530

    removed terraform plans

--- terraform/modules/digital-ocean/provider.tf
+++
@@ -1,7 +0,0 @@
-variable "do_token" {
-  description = "Access token for managing resources on DigitalOcean with write access"
-}
-
-provider "digitalocean" {
-  token = var.do_token
-}
--- terraform/modules/kubernetes/doppler-kubernetes-operator.tf
+++
@@ -1,5 +0,0 @@
-resource "helm_release" "doppler_kubernetes_operator" {
-  name       = "doppler-kubernetes-operator"
-  repository = "https://helm.doppler.com"
-  chart      = "doppler-kubernetes-operator"
-}
