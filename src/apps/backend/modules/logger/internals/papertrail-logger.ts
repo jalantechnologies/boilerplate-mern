6fcc9e44923f79e88eae321b41c3e12fb0a3f4c4
bbbneo333 <ankit@jalantechnologies.com>
2023-08-17 15:35:22 +0530

    fix: fixed port config parsing

--- src/apps/backend/modules/logger/internals/papertrail-logger.ts
+++ src/apps/backend/modules/logger/internals/papertrail-logger.ts
@@ -1,13 +1,14 @@
 import { PapertrailTransport } from 'winston-papertrail-transport';
 
 import ConfigService from '../../config/config-service';
+
 import WinstonLogger from './winston-logger';
 
 export default class PapertrailLogger extends WinstonLogger {
   constructor() {
     const transport = new PapertrailTransport({
       host: ConfigService.getStringValue('papertrail.host'),
-      port: ConfigService.getIntValue('papertrail.port'),
+      port: parseInt(ConfigService.getStringValue('papertrail.port'), 10),
       program: ConfigService.hasValue('papertrail.program')
         ? ConfigService.getStringValue('papertrail.program')
         : 'default',
