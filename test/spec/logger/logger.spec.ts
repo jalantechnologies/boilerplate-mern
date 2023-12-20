5b0328e6dad005f60c24b31fcb56cf10594bc9f2
chimedpalden <chimedpaldan@gmail.com>
2023-12-20 12:59:17 +0530

    feat: add datadog spec

--- test/spec/logger/logger.spec.ts
+++ test/spec/logger/logger.spec.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import LoggerManager from '../../../src/apps/backend/modules/logger/logger-manager';
 import Logger from '../../../src/apps/backend/modules/logger/logger';
 import ConfigService from '../../../src/apps/backend/modules/config/config-service';
+import DatadogLogger from '../../../src/apps/backend/modules/logger/internals/datadog-logger';
 import ConsoleLogger from '../../../src/apps/backend/modules/logger/internals/console-logger';
 import Loggers from '../../../src/apps/backend/modules/logger/internals/loggers';
 import { LoggerTransport } from '../../../src/apps/backend/modules/logger/internals/types';
@@ -11,6 +12,7 @@ import { LoggerTransport } from '../../../src/apps/backend/modules/logger/intern
 describe('Logger', () => {
   let sinonSandbox: sinon.SinonSandbox;
   const consoleLogger = new ConsoleLogger();
+  const datadogLogger = new DatadogLogger();
 
   beforeEach(() => {
     sinonSandbox = sinon.createSandbox();
@@ -35,6 +37,19 @@ describe('Logger', () => {
     sinonSandbox.stub(Loggers, 'getConsoleLogger').returns(consoleLogger);
 
     const stub = sinonSandbox.stub(consoleLogger, 'info');
+    LoggerManager.mountLogger();
+    Logger.info('test');
+
+    expect(stub.calledOnce).to.be.true;
+  });
+
+  it('should be able to register datadog as a logger', () => {
+    sinonSandbox
+      .stub(ConfigService, 'getListValue')
+      .returns([LoggerTransport.Datadog]);
+    sinonSandbox.stub(Loggers, 'getDatadogLogger').returns(datadogLogger);
+
+    const stub = sinonSandbox.stub(datadogLogger, 'info');
     LoggerManager.mountLogger();
     Logger.info('test');
 
