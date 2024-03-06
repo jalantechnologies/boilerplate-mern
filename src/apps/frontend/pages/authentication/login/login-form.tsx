f8055f506a944f0892e1c7f83cf198fed3886219
Krishna Vamsi <krishna.vamsi@jalantechnologies.com>
2024-03-06 21:15:00 +0530

    form header fix

--- src/apps/frontend/pages/authentication/login/login-form.tsx
+++ src/apps/frontend/pages/authentication/login/login-form.tsx
@@ -24,103 +24,99 @@ const LoginForm: React.FC<LoginFormProps> = ({ onError, onSuccess }) => {
 
   return (
     <>
-     <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
-        <h3 className="font-medium text-black dark:text-white">
-          Sign In
-        </h3>
-      </div>
+      <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
+        Sign In
+      </h2>
       <form onSubmit={formik.handleSubmit}>
-        <div className="p-6.5">
-          <div className="mb-4.5">
-            <FormControl
-              label={'Email'}
-              error={formik.touched.username && formik.errors.username}
-            >
-              <Input
-                data-testid="username"
-                disabled={isLoginLoading}
-                error={formik.touched.username && formik.errors.username}
-                name="username"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                placeholder="Enter your email"
-                value={formik.values.username}
-              />
-              <span className="absolute right-4 top-4.5">
-                <img
-                  className="fill-current opacity-50"
-                  src="/assets/img/icon/email.svg"
-                  alt="email icon"
-                />
-              </span>
-            </FormControl>
-          </div>
-
-          <div>
-            <FormControl
-              label={'Password'}
-              error={formik.touched.password && formik.errors.password}
-            >
-              <Input
-                data-testid="password"
-                disabled={isLoginLoading}
-                error={formik.touched.password && formik.errors.password}
-                name="password"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                placeholder="Enter your password"
-                type={isPasswordVisible ? 'text' : 'password'}
-                value={formik.values.password}
-              />
-
-              <button
-                className="absolute inset-y-0 right-0 flex items-center px-4"
-                onClick={(e) => {
-                  e.preventDefault();
-                  togglePasswordVisibility();
-                }}
-              >
-                {isPasswordVisible ? (
-                  <img
-                    className="size-6.5 opacity-65"
-                    src="/assets/img/icon/eye-closed.svg"
-                    alt="hide password icon"
-                  />
-                ) : (
-                  <img
-                    className="size-6.5 opacity-65"
-                    src="/assets/img/icon/eye-open.svg"
-                    alt="show password icon"
-                  />
-                )}
-              </button>
-            </FormControl>
-          </div>
-
-          <div className="mb-5.5 mt-5 flex items-center justify-between">
-            <label htmlFor="formCheckbox" className="flex cursor-pointer">
-              <LoginFormCheckbox />
-              <p>Remember me</p>
-            </label>
-
-            <a href="#" className="text-sm text-primary hover:underline">Forget password?</a>
-          </div>
-
-          <button
-            className={`w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-primary/90 active:bg-primary/80
-              ${isLoginLoading && 'cursor-not-allowed bg-primary/80 hover:bg-primary/80'}`
-            }
-            disabled={isLoginLoading}
-            type="submit"
+        <div className="mb-4.5">
+          <FormControl
+            label={'Email'}
+            error={formik.touched.username && formik.errors.username}
           >
-            Sign In
-          </button>
-          <div className='mt-4 flex justify-end'>
-            Don't have an account?&nbsp;
-            <Link to={constants.SIGNUP} className="text-primary">
-              Sign Up
-            </Link>
-          </div>
+            <Input
+              data-testid="username"
+              disabled={isLoginLoading}
+              error={formik.touched.username && formik.errors.username}
+              name="username"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Enter your email"
+              value={formik.values.username}
+            />
+            <span className="absolute right-4 top-4.5">
+              <img
+                className="fill-current opacity-50"
+                src="/assets/img/icon/email.svg"
+                alt="email icon"
+              />
+            </span>
+          </FormControl>
+        </div>
+
+        <div>
+          <FormControl
+            label={'Password'}
+            error={formik.touched.password && formik.errors.password}
+          >
+            <Input
+              data-testid="password"
+              disabled={isLoginLoading}
+              error={formik.touched.password && formik.errors.password}
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Enter your password"
+              type={isPasswordVisible ? 'text' : 'password'}
+              value={formik.values.password}
+            />
+
+            <button
+              className="absolute inset-y-0 right-0 flex items-center px-4"
+              onClick={(e) => {
+                e.preventDefault();
+                togglePasswordVisibility();
+              }}
+            >
+              {isPasswordVisible ? (
+                <img
+                  className="size-6.5 opacity-65"
+                  src="/assets/img/icon/eye-closed.svg"
+                  alt="hide password icon"
+                />
+              ) : (
+                <img
+                  className="size-6.5 opacity-65"
+                  src="/assets/img/icon/eye-open.svg"
+                  alt="show password icon"
+                />
+              )}
+            </button>
+          </FormControl>
+        </div>
+
+        <div className="mb-5.5 mt-5 flex items-center justify-between">
+          <label htmlFor="formCheckbox" className="flex cursor-pointer">
+            <LoginFormCheckbox />
+            <p>Remember me</p>
+          </label>
+
+          <a href="#" className="text-sm text-primary hover:underline">Forget password?</a>
+        </div>
+
+        <button
+          className={`w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-primary/90 active:bg-primary/80
+            ${isLoginLoading && 'cursor-not-allowed bg-primary/80 hover:bg-primary/80'}`
+          }
+          disabled={isLoginLoading}
+          type="submit"
+        >
+          Sign In
+        </button>
+        <div className='mt-4 flex justify-end'>
+          Don't have an account?&nbsp;
+          <Link to={constants.SIGNUP} className="text-primary">
+            Sign Up
+          </Link>
         </div>
       </form>
     </>
--- src/apps/frontend/pages/authentication/signup/signup-form.tsx
+++ src/apps/frontend/pages/authentication/signup/signup-form.tsx
@@ -28,167 +28,163 @@ const SignupForm: React.FC<SignupFormProps> = ({ onError, onSuccess }) => {
 
   return (
     <>
-     <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
-        <h3 className="font-medium text-black dark:text-white">
-          Sign Up
-        </h3>
-      </div>
+      <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
+        Sign Up
+      </h2>
       <form onSubmit={formik.handleSubmit}>
-        <div className="p-6.5">
-          <div className="mb-4.5 flex flex-col gap-6 2sm:flex-row">
-            <div className="w-full 2sm:w-1/2">
-              <FormControl
-                label={'First name'}
+        <div className="mb-4.5 flex flex-col gap-6 2sm:flex-row">
+          <div className="w-full 2sm:w-1/2">
+            <FormControl
+              label={'First name'}
+              error={formik.touched.firstName && formik.errors.firstName}
+            >
+              <Input
                 error={formik.touched.firstName && formik.errors.firstName}
-              >
-                <Input
-                  error={formik.touched.firstName && formik.errors.firstName}
-                  data-testid="firstName"
-                  disabled={isSignupLoading}
-                  name="firstName"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  placeholder="Enter your first name"
-                  value={formik.values.firstName}
-                />
-              </FormControl>
-            </div>
-            <div className="w-full 2sm:w-1/2">
-              <FormControl
-                label={'Last name'}
+                data-testid="firstName"
+                disabled={isSignupLoading}
+                name="firstName"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                placeholder="Enter your first name"
+                value={formik.values.firstName}
+              />
+            </FormControl>
+          </div>
+          <div className="w-full 2sm:w-1/2">
+            <FormControl
+              label={'Last name'}
+              error={formik.touched.lastName && formik.errors.lastName}
+            >
+              <Input
                 error={formik.touched.lastName && formik.errors.lastName}
-              >
-                <Input
-                  error={formik.touched.lastName && formik.errors.lastName}
-                  data-testid="lastName"
-                  disabled={isSignupLoading}
-                  name="lastName"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  placeholder="Enter your last name"
-                  value={formik.values.lastName}
-                />
-              </FormControl>
-            </div>
-          </div>
-          <div className="mb-4.5">
-            <FormControl
-              label={'Email'}
-              error={formik.touched.username && formik.errors.username}
-            >
-              <Input
-                error={formik.touched.username && formik.errors.username}
-                data-testid="username"
+                data-testid="lastName"
                 disabled={isSignupLoading}
-                name="username"
+                name="lastName"
                 onBlur={formik.handleBlur}
                 onChange={formik.handleChange}
-                placeholder="Enter your email"
-                value={formik.values.username}
+                placeholder="Enter your last name"
+                value={formik.values.lastName}
               />
-              <span className="absolute right-4 top-4.5">
-                <img
-                  alt="email icon"
-                  className="fill-current opacity-50"
-                  src="/assets/img/icon/email.svg"
-                />
-              </span>
             </FormControl>
           </div>
-          <div className="mb-4.5">
-            <FormControl
-              label={'Password'}
-              error={formik.touched.password && formik.errors.password}
-            >
-              <Input
-                error={formik.touched.password && formik.errors.password}
-                data-testid="password"
-                disabled={isSignupLoading}
-                name="password"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                placeholder="Enter your password"
-                type={isPasswordVisible ? 'text' : 'password'}
-                value={formik.values.password}
-              />
-              <button
-                className="absolute inset-y-0 right-0 flex items-center px-4"
-                onClick={(e) => {
-                  e.preventDefault();
-                  togglePasswordVisibility();
-                }}
-              >
-                {isPasswordVisible ? (
-                  <img
-                    alt="hide password icon"
-                    src="/assets/img/icon/eye-closed.svg"
-                    className="size-6.5 opacity-65"
-                  />
-                ) : (
-                  <img
-                    alt="show password icon"
-                    src="/assets/img/icon/eye-open.svg"
-                    className="size-6.5 opacity-65"
-                  />
-                )}
-              </button>
-            </FormControl>
-          </div>
-          <div className="mb-5.5">
-            <FormControl
-              label={'Re-type Password'}
-              error={formik.touched.retypePassword && formik.errors.retypePassword}
-            >
-              <Input
-                error={formik.touched.retypePassword && formik.errors.retypePassword}
-                data-testid="retypePassword"
-                disabled={isSignupLoading}
-                name="retypePassword"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                placeholder="Re-enter the password"
-                type={isRetypePasswordVisible ? 'text' : 'password'}
-                value={formik.values.retypePassword}
-              />
-              <button
-                className="absolute inset-y-0 right-0 flex items-center px-4"
-                onClick={(e) => {
-                  e.preventDefault();
-                  toggleRetypePasswordVisibility();
-                }}
-              >
-                {isRetypePasswordVisible ? (
-                  <img
-                    alt="hide password icon"
-                    src="/assets/img/icon/eye-closed.svg"
-                    className="size-6.5 opacity-65"
-                  />
-                ) : (
-                  <img
-                    alt="show password icon"
-                    src="/assets/img/icon/eye-open.svg"
-                    className="size-6.5 opacity-65"
-                  />
-                )}
-              </button>
-            </FormControl>
-          </div>
-
-          <button
-            className={`w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-primary/90 active:bg-primary/80
-              ${isSignupLoading && 'cursor-not-allowed bg-primary/80 hover:bg-primary/80'}`
-            }
-            disabled={isSignupLoading}
-            type="submit"
+        </div>
+        <div className="mb-4.5">
+          <FormControl
+            label={'Email'}
+            error={formik.touched.username && formik.errors.username}
           >
-            Sign Up
-          </button>
-          <div className='mt-4 flex justify-end'>
-            Already have an account?&nbsp;
-            <Link to={constants.LOGIN} className="text-primary">
-              Sign In
-            </Link>
-          </div>
+            <Input
+              error={formik.touched.username && formik.errors.username}
+              data-testid="username"
+              disabled={isSignupLoading}
+              name="username"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              placeholder="Enter your email"
+              value={formik.values.username}
+            />
+            <span className="absolute right-4 top-4.5">
+              <img
+                alt="email icon"
+                className="fill-current opacity-50"
+                src="/assets/img/icon/email.svg"
+              />
+            </span>
+          </FormControl>
+        </div>
+        <div className="mb-4.5">
+          <FormControl
+            label={'Password'}
+            error={formik.touched.password && formik.errors.password}
+          >
+            <Input
+              error={formik.touched.password && formik.errors.password}
+              data-testid="password"
+              disabled={isSignupLoading}
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              placeholder="Enter your password"
+              type={isPasswordVisible ? 'text' : 'password'}
+              value={formik.values.password}
+            />
+            <button
+              className="absolute inset-y-0 right-0 flex items-center px-4"
+              onClick={(e) => {
+                e.preventDefault();
+                togglePasswordVisibility();
+              }}
+            >
+              {isPasswordVisible ? (
+                <img
+                  alt="hide password icon"
+                  src="/assets/img/icon/eye-closed.svg"
+                  className="size-6.5 opacity-65"
+                />
+              ) : (
+                <img
+                  alt="show password icon"
+                  src="/assets/img/icon/eye-open.svg"
+                  className="size-6.5 opacity-65"
+                />
+              )}
+            </button>
+          </FormControl>
+        </div>
+        <div className="mb-5.5">
+          <FormControl
+            label={'Re-type Password'}
+            error={formik.touched.retypePassword && formik.errors.retypePassword}
+          >
+            <Input
+              error={formik.touched.retypePassword && formik.errors.retypePassword}
+              data-testid="retypePassword"
+              disabled={isSignupLoading}
+              name="retypePassword"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              placeholder="Re-enter the password"
+              type={isRetypePasswordVisible ? 'text' : 'password'}
+              value={formik.values.retypePassword}
+            />
+            <button
+              className="absolute inset-y-0 right-0 flex items-center px-4"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleRetypePasswordVisibility();
+              }}
+            >
+              {isRetypePasswordVisible ? (
+                <img
+                  alt="hide password icon"
+                  src="/assets/img/icon/eye-closed.svg"
+                  className="size-6.5 opacity-65"
+                />
+              ) : (
+                <img
+                  alt="show password icon"
+                  src="/assets/img/icon/eye-open.svg"
+                  className="size-6.5 opacity-65"
+                />
+              )}
+            </button>
+          </FormControl>
+        </div>
+
+        <button
+          className={`w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-primary/90 active:bg-primary/80
+            ${isSignupLoading && 'cursor-not-allowed bg-primary/80 hover:bg-primary/80'}`
+          }
+          disabled={isSignupLoading}
+          type="submit"
+        >
+          Sign Up
+        </button>
+        <div className='mt-4 flex justify-end'>
+          Already have an account?&nbsp;
+          <Link to={constants.LOGIN} className="text-primary">
+            Sign In
+          </Link>
         </div>
       </form>
     </>
