a27b7cd248fb59b61ea12a5b8bb945c50e05aa0a
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-03-07 08:41:24 +0530

    configured postcss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,32 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack',
+    ({
+      name: "@storybook/addon-styling-webpack",
+
+      options: {
+        rules: [{
+      test: /\.css$/,
+      sideEffects: true,
+      use: [
+          require.resolve("style-loader"),
+          {
+              loader: require.resolve("css-loader"),
+              options: {
+                  
+                  importLoaders: 1,
+              },
+          },{
+    loader: require.resolve("postcss-loader"),
+    options: {
+    implementation: require.resolve("postcss"),
+    },
+    },
+      ],
+    },],
+      }
+    })
   ],
   framework: {
     name: '@storybook/react-webpack5',
