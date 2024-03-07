8277df2cf9ff211656fb37987fc91ce0f10c27bc
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-03-07 08:41:24 +0530

    configured postcss

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
 
+import '../src/apps/frontend/style.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
