4b5c1238d500ce3fd497d714c1f49027ba6e43ac
dev-rathore <rathoredvd3001@gmail.com>
2024-03-08 15:55:35 +0530

    added test cases

--- test/spec/access-token/access-token.spec.ts
+++ test/spec/access-token/access-token.spec.ts
@@ -1,10 +1,26 @@
 import faker from '@faker-js/faker';
 import chai, { expect } from 'chai';
+import sinon from 'sinon';
 
 import AccountWriter from '../../../src/apps/backend/modules/account/internal/account-writer';
 import { app } from '../../helpers/app';
+import AccountRepository from '../../../src/apps/backend/modules/account/internal/store/account-repository';
+import { OtpService } from '../../../src/apps/backend/modules/otp';
+import { SMSService } from '../../../src/apps/backend/modules/communication';
 
 describe('AccessToken API', () => {
+  let sinonSandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    sinonSandbox = sinon.createSandbox();
+  });
+  
+  afterEach(async () => {
+    sinonSandbox.restore();
+
+    await AccountRepository.deleteMany();
+  });
+
   describe('POST /access-tokens', () => {
     it('should return access token for given username password', async () => {
       const params = {
@@ -25,6 +41,33 @@ describe('AccessToken API', () => {
         .post('/api/access-tokens')
         .set('content-type', 'application/json')
         .send(params);
+
+      expect(res.body.token).to.be.a('string');
+    });
+
+    it('should return access token for given contact number', async () => {
+      const contactNumber = {
+        countryCode: '+91',
+        phoneNumber: '9834567890',
+      };
+
+      sinonSandbox.stub(SMSService, <any>'sendSMS').returns(Promise.resolve(true));
+
+      await AccountWriter.createAccountByContactNumber({
+        countryCode: contactNumber.countryCode,
+        phoneNumber: contactNumber.phoneNumber,
+      });
+
+      const otp = await OtpService.createOtp(contactNumber);
+
+      const res = await chai
+        .request(app)
+        .post('/api/access-tokens')
+        .set('content-type', 'application/json')
+        .send({
+          contactNumber,
+          otpCode: otp.otpCode,
+        });
 
       expect(res.body.token).to.be.a('string');
     });
--- test/spec/account/account.spec.ts
+++ test/spec/account/account.spec.ts
@@ -1,11 +1,23 @@
 import faker from '@faker-js/faker';
 import chai, { expect } from 'chai';
+import sinon from 'sinon';
 
 import { AccountWithUserNameExistsError } from '../../../src/apps/backend/modules/account';
 import AccountWriter from '../../../src/apps/backend/modules/account/internal/account-writer';
+import { SMSService } from '../../../src/apps/backend/modules/communication';
 import { app } from '../../helpers/app';
 
 describe('Account API', () => {
+  let sinonSandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    sinonSandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sinonSandbox.restore();
+  });
+
   describe('POST /accounts', () => {
     it('should create a new account', async () => {
       const params = {
@@ -31,7 +43,12 @@ describe('Account API', () => {
         username: faker.internet.userName(),
         password: 'password',
       };
-      await AccountWriter.createAccount(params);
+      await AccountWriter.createAccountByUsernameAndPassword(
+        params.firstName,
+        params.lastName,
+        params.password,
+        params.username,
+      );
       const res = await chai
         .request(app)
         .post('/api/accounts')
@@ -42,6 +59,75 @@ describe('Account API', () => {
       expect(res.body.message).to.eq(
         new AccountWithUserNameExistsError(params.username).message,
       );
+    });
+
+    it('should create a new account if it does not exist and send otp when user signing up through contact number ', async () => {
+      const contactNumber = {
+        countryCode: '+91',
+        phoneNumber: '7895586769',
+      };
+
+      const sendSMSStub = sinonSandbox.stub(SMSService, <any>'sendSMS').returns(Promise.resolve(true));
+
+      const res = await chai
+        .request(app)
+        .post('/api/accounts')
+        .set('content-type', 'application/json')
+        .send({
+          contactNumber,
+        });
+
+      expect(res.status).to.be.eq(201);
+      expect(res.body.contactNumber.countryCode).to.eq(contactNumber.countryCode);
+      expect(res.body.contactNumber.phoneNumber).to.eq(contactNumber.phoneNumber);
+      expect(sendSMSStub.calledOnce).to.be.true;
+    });
+
+    it('should send otp if account already exists when user signing up through contact number', async () => {
+      const contactNumber = {
+        countryCode: '+91',
+        phoneNumber: '7895586769',
+      };
+
+      await AccountWriter.createAccountByContactNumber(contactNumber);
+
+      const createAccountByContactNumberStub = sinonSandbox.stub(AccountWriter, <any>'createAccountByContactNumber').returns(Promise.resolve(true));
+      const sendSMSStub = sinonSandbox.stub(SMSService, <any>'sendSMS').returns(Promise.resolve(true));
+
+      const res = await chai
+        .request(app)
+        .post('/api/accounts')
+        .set('content-type', 'application/json')
+        .send({
+          contactNumber,
+        });
+
+      expect(res.status).to.be.eq(201);
+      expect(res.body.contactNumber.countryCode).to.eq(contactNumber.countryCode);
+      expect(res.body.contactNumber.phoneNumber).to.eq(contactNumber.phoneNumber);
+      expect(sendSMSStub.calledOnce).to.be.true;
+      expect(createAccountByContactNumberStub.calledOnce).to.be.false;
+    });
+
+    it('should throw error when creating a new account with invalid contact number', async () => {
+      const contactNumber = {
+        countryCode: '+1',
+        phoneNumber: '7895586',
+      };
+
+      const sendSMSStub = sinonSandbox.stub(SMSService, <any>'sendSMS').returns(Promise.resolve(true));
+
+      const res = await chai
+        .request(app)
+        .post('/api/accounts')
+        .set('content-type', 'application/json')
+        .send({
+          contactNumber,
+        });
+
+      expect(res.status).to.be.eq(500);
+      expect(res.body.message).to.eq('Invalid contact number');
+      expect(sendSMSStub.calledOnce).to.be.false;
     });
   });
 });
