678d5b0faca5a8b2dba4ef97e39979a7b75fcf64
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-03-11 10:27:48 +0530

    code formatting improved

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,31 +8,33 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-styling-webpack',
-    ({
-      name: "@storybook/addon-styling-webpack",
+    {
+      name: '@storybook/addon-styling-webpack',
 
       options: {
-        rules: [{
-      test: /\.css$/,
-      sideEffects: true,
-      use: [
-          require.resolve("style-loader"),
+        rules: [
           {
-              loader: require.resolve("css-loader"),
-              options: {
-                  
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
                   importLoaders: 1,
+                },
               },
-          },{
-    loader: require.resolve("postcss-loader"),
-    options: {
-    implementation: require.resolve("postcss"),
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  implementation: require.resolve('postcss'),
+                },
+              },
+            ],
+          },
+        ],
+      },
     },
-    },
-      ],
-    },],
-      }
-    })
   ],
   framework: {
     name: '@storybook/react-webpack5',
