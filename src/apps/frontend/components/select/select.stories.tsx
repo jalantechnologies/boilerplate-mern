bd67348da3d4ddcc56d7d719707f411f713e1374
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2024-03-15 08:33:09 +0530

    Add Select component stories

---
+++ src/apps/frontend/components/select/select.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Select from '.';
+
+const options = [
+  { value: '1', label: 'Option 1' },
+  { value: '2', label: 'Option 2' },
+  { value: '3', label: 'Option 3' },
+];
+
+export default {
+  args: {
+    options,
+  },
+  argTypes: {
+    isLoading: { control: 'boolean' },
+    multiple: { control: 'boolean' },
+    value: { control: 'text' },
+  },
+  component: Select,
+  title: 'Components/Select',
+} as Meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {};
+
+export const Multiple: Story = {
+  args: {
+    multiple: true,
+  },
+};
+
+export const PreSelected: Story = {
+  args: {
+    value: '3',
+  },
+};
+
+export const WithLoading: Story = {
+  args: {
+    isLoading: true,
+  },
+};
