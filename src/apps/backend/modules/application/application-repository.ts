a16793a4cd8f13ab7b7e0cdfdc91ec2f247576ff
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2024-04-18 17:42:17 +0530

    Improved Index Management in MongoDB (#123)
    * added syncIndex
    
    * lint fix
    
    ---------
    
    Co-authored-by: Pankaj Kumar Verma <pankaj@Pankajs-MacBook-Pro.local>

--- src/apps/backend/modules/application/application-repository.ts
+++ src/apps/backend/modules/application/application-repository.ts
@@ -27,6 +27,9 @@ export default function ApplicationRepository<T>(
 ): mongoose.Model<T> {
   const connection = getDatabaseConnection();
 
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  connection.syncIndexes();
+
   // TODO: Typings for repositories is not working as expected, this needs
   //  to be fixed.
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
