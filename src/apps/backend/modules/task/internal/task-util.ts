abec67a377a51ad106997ec80901fd131388ac86
Shubham Aggarwal <shubhamaggarwal0812@gmail.com>
2024-06-16 15:43:43 +0530

    feat: Update TaskUtil to include sharedWith field in task conversion

--- src/apps/backend/modules/task/internal/task-util.ts
+++ src/apps/backend/modules/task/internal/task-util.ts
@@ -9,6 +9,7 @@ export default class TaskUtil {
     task.account = taskDb.account.toString();
     task.description = taskDb.description;
     task.title = taskDb.title;
+    task.sharedWith = taskDb.sharedWith ? taskDb.sharedWith.map((id) => id.toString()) : [];
     return task;
   }
 }
