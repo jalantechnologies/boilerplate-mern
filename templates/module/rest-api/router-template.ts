679c9016362ed2129a05e9037b08748102d3b653
Govind Sharma <govindgovind852@gmail.com>
2024-10-02 11:46:47 +0530

    feat: allow adding a baseurl to the user

--- templates/module/rest-api/router-template.ts
+++ templates/module/rest-api/router-template.ts
@@ -10,13 +10,15 @@ export default class $entityNameRouter extends ApplicationRouter {
 
     router.use(accessAuthMiddleware);
 
+    const baseUrl='$baseUrl'
+
     // Define get routes
-    router.get('/', ctrl.get$entityName);
-    router.get('/:id', ctrl.get$entityNameById);
+    router.get(`${baseUrl}/`, ctrl.get$entityName);
+    router.get(`${baseUrl}/:id`, ctrl.get$entityNameById);
 
     // Define post routes here
-    router.post('/', ctrl.create$entityName);
-    router.patch('/:id', ctrl.update$entityName);
-    router.delete('/:id', ctrl.delete$entityName);
+    router.post(`${baseUrl}/`, ctrl.create$entityName);
+    router.patch(`${baseUrl}/:id`, ctrl.update$entityName);
+    router.delete(`${baseUrl}/:id`, ctrl.delete$entityName);
   }
 }
