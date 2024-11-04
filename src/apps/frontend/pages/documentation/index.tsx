9b60e2e530f69c6756b5e3cef69de4aa142b54c1
krishna_vamsi <krishna.vamsi@jalantechnologies.com>
2024-11-04 13:51:20 +0530

    enhance api documentation's markdown displaying UI to have proper formatting

--- src/apps/frontend/pages/documentation/index.tsx
+++ src/apps/frontend/pages/documentation/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import markdownContent from '../../../../assets/documentation/index.md';
+import 'github-markdown-css/github-markdown.css';
+import './custom-markdown-styles.scss';
 
 const Documentation: React.FC = () => (
-  <div className="p-4 text-black">
+  <div className="markdown-body">
     <ReactMarkdown>{markdownContent}</ReactMarkdown>
   </div>
 );
