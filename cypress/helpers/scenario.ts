ce04ddd8da29da230338306593af73549d0162be
Sayan Bhattacharyya <sayan.bhattacharyya@bettrsw.com>
2025-02-12 18:33:41 +0530

    fix(cypress): return chainable from scenario to properly wait for data

--- cypress/helpers/scenario.ts
+++ cypress/helpers/scenario.ts
@@ -1,22 +1,4 @@
 export const setupScenario = <T = unknown>(
   name: string,
   params?: unknown,
-): T => {
-  let result: T;
-
-  beforeEach((done) => {
-    cy.task('scenario:setup', { name, params }).then((response: T) => {
-      result = response;
-      done();
-    });
-  });
-
-  afterEach((done) => {
-    cy.task('scenario:cleanup', { name }).then(() => {
-      result = null;
-      done();
-    });
-  });
-
-  return result;
-};
+): Cypress.Chainable<T> => cy.task('scenario:setup', { name, params });
