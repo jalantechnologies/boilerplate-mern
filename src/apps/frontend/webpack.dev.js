f08456b2532c1facf50f54438cf8fa5e6ee16a90
Sayan Bhattacharyya <sayan.bhattacharyya@bettrsw.com>
2025-02-14 18:42:51 +0530

    fix(webpack): wait for backend to get ready (#206)
    * fix(webpack): wait for backend to get ready
    
    * chore: restructure constants

--- src/apps/frontend/webpack.dev.js
+++ src/apps/frontend/webpack.dev.js
@@ -1,4 +1,6 @@
 const { merge } = require('webpack-merge');
+const http = require('http');
+const open = require('open');
 
 const baseConfig = require('./webpack.base');
 
@@ -6,7 +8,38 @@ const devServerOpen = process.env.WEBPACK_DEV_DISABLE_OPEN !== 'true';
 const devServerPort = 3000;
 const devServerAPIProxyPort = 8080;
 
+const API_SERVER_URL = `http://localhost:${devServerAPIProxyPort}`;
+const FRONTEND_URL = `http://localhost:${devServerPort}`;
 const DOCUMENTATION_PAGE_ROUTE = '/api/documentation';
+const RETRY_INTERVAL_IN_MS = 2000;
+
+function waitForBackend(port, callback) {
+  function check() {
+    const req = http.request(
+      { method: 'HEAD', host: 'localhost', port },
+      (res) => {
+        if (res.statusCode < 500) {
+          console.log(`Backend is ready on port ${port}`);
+          callback();
+        } else {
+          console.log(`Waiting for backend on port ${port}...`);
+          setTimeout(check, RETRY_INTERVAL_IN_MS);
+        }
+      }
+    );
+
+    req.on('error', () => {
+      console.log(
+        `Backend not ready, retrying in ${RETRY_INTERVAL_IN_MS / 1000} seconds...`
+      );
+      setTimeout(check, RETRY_INTERVAL_IN_MS);
+    });
+
+    req.end();
+  }
+
+  check();
+}
 
 const config = {
   mode: 'development',
@@ -20,13 +53,13 @@ const config = {
   devServer: {
     historyApiFallback: true,
     hot: true,
-    open: devServerOpen,
+    open: false,
     port: devServerPort,
     proxy: {
       secure: false,
       // Forwards requests with /api to server running on the devServerAPIProxyPort
       '/api': {
-        target: `http://localhost:${devServerAPIProxyPort}`,
+        target: API_SERVER_URL,
         bypass: function (req) {
           // To place the documentation page on the respective route, the proxy should be bypassed for this route.
           if (req.url === DOCUMENTATION_PAGE_ROUTE) {
@@ -34,7 +67,21 @@ const config = {
           }
         },
       },
-      '/assets': `http://localhost:${devServerAPIProxyPort}`,
+      '/assets': API_SERVER_URL,
+    },
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('Webpack DevServer is not defined');
+      }
+
+      waitForBackend(devServerAPIProxyPort, () => {
+        console.log('Starting Webpack Dev Server...');
+        if (devServerOpen) {
+          open(FRONTEND_URL);
+        }
+      });
+
+      return middlewares;
     },
   },
 };
